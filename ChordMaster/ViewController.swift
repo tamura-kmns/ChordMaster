cb2e46023ec00eafb0379aa762d6b2df78800cad
KatsuhikoTamura <tamura@9bit.jp.net>
2018-07-03 04:46:53 +0900

    Initial Commit

---
+++ ChordMaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChordMaster
+//
+//  Created by KatsuhikoTamura on 2018/07/03.
+//  Copyright © 2018年 9bit.co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
